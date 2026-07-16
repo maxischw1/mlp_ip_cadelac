2fae5d2b620ba02cb44637a08973207d4911b8e1
maxischw1 <maxi.schw@icloud.com>
2026-07-16 08:50:51 +0200

    Set exoskeleton training to 3000 epochs

--- cadelac/learning/train_panda.py
+++ cadelac/learning/train_panda.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
              'n_lstm_depth': n_lstm_depth,
              'hist_length': hist_length,
              'act_ld': 'Softplus',
-             'max_epoch': 1000
+             'max_epoch': 3000
             }
 
     model_name = 'epochs_' + str(hyper['max_epoch'])
